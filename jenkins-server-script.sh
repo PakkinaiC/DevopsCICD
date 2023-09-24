efffa563c34a5a617ceacb6241ce3ffe9bd37f00
pakkinai chanthavuttikorn <pakkinaichanthavuttigorn@gmail.com>
2023-09-24 20:20:32 +0700

    Re-configure wget jenkin command

--- jenkins-server-script.sh
+++ jenkins-server-script.sh
@@ -3,8 +3,7 @@
 # install jenkins
 
 sudo yum update
-sudo wget -O /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 sudo yum upgrade -y
 sudo amazon-linux-extras install java-openjdk11 -y
